e73f74dc42495fb79c019333194114fddbd19ce8
Matheus Vilela <matheus.vilela.898@gmail.com>
2026-04-27 14:07:23 -0300

    refactor: enhance solicitacao-item with CRUD actions
    - Add edit button to navigate to edit form
    - Add delete button with facade.deletar() integration
    - Add analyze/detail button with stop propagation
    - Style action buttons with distinct colors (edit=blue, analyze=purple, delete=red)
    - Refactor from dumb to smart component with facade integration

--- src/app/components/solicitacoes/solicitacao-item/solicitacao-item.ts
+++ src/app/components/solicitacoes/solicitacao-item/solicitacao-item.ts
@@ -1,7 +1,9 @@
-import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+
 import { SolicitacaoViewModel } from '../../../services/graphql.service';
+import { SolicitacoesFacade } from '../../../services/solicitacoes.facade';
 
 @Component({
   selector: 'app-solicitacao-item',
@@ -14,9 +16,20 @@ import { SolicitacaoViewModel } from '../../../services/graphql.service';
 export class SolicitacaoItemComponent {
   @Input({ required: true }) solicitacao!: SolicitacaoViewModel;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private facade: SolicitacoesFacade,
+  ) {}
 
   irParaDetalhe() {
     this.router.navigate(['/solicitacoes', this.solicitacao.id]);
+  }
+
+  editar() {
+    this.router.navigate(['/solicitacoes/editar', this.solicitacao.id]);
+  }
+
+  deletar() {
+    this.facade.deletar(this.solicitacao.id);
   }
 }
